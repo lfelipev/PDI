05d4271101f813b8382a20ddebdce7c6dcd9a6fc
lfelipev <felipemmha@gmail.com>
2018-02-08 00:52:39 -0300

    filtros

--- filtros.py
+++ filtros.py
@@ -19,9 +19,29 @@ def medianBlurring():
     plt.subplot(122), plt.imshow(median), plt.title('Filtered')
     plt.xticks([]), plt.yticks([])    
     plt.show()
+
+def gaussianBlurring():
+    img = cv2.imread('105053.png')
+    median = cv2.GaussianBlur(img, (5,5), 0)
+    plt.subplot(121), plt.imshow(img), plt.title('Original')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(median), plt.title('Filtered')
+    plt.xticks([]), plt.yticks([])    
+    plt.show()
+
+def bilateralFilter():
+    img = cv2.imread('105053.png')
+    median = cv2.bilateralFilter(img, 9, 75, 75)
+    plt.subplot(121), plt.imshow(img), plt.title('Original')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(median), plt.title('Filtered')
+    plt.xticks([]), plt.yticks([])    
+    plt.show()
     
 ## Comentar e descomentar as funções na main() para testá-las individualmente
 def main():
+    #bilateralFilter()
+    #aussianBlurring()
     medianBlurring()
         
 if __name__ == '__main__':
